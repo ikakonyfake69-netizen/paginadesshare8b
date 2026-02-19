893e5b93d93124fdf02a1fd8c85c3545af6e7fb7
FreddyGP24 <freddygp1822@gmail.com>
2026-02-19 17:51:11 -0600

    Add index page with team member CV links and logo

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+    <h1>Bienvenido al sistema</h1>
+    <br>
+    <img src="./image.png" alt="Logo del sistema" width="200" height="200">
+    <br>
+    <h2>Scrum Team</h2>
+    <ul>
+        <li><a href="./freddy.docx">Descargar CV de Freddy</a></li>
+        <li><a href="./brayan.docx">Descargar CV de Brayan</a></li>
+        <li><a href="./enrique.docx">Descargar CV de Enrique</a></li>
+        <li><a href="./mike.docx">Descargar CV de Luis miguel</a></li>
+        <li><a href="./checo.docx">Descargar CV de Sergio</a></li>
+    </ul>
+    </center>
+</body>
+</html>
